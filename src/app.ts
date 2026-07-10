7e484b357fae1a1bc42db4532221e054ca1955e4
okorokosi042 <okorpkosi@gmail.com>
2026-07-10 04:59:04 +0100

    Implement user authentication with Prisma bcrypt and JWT

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,16 @@
 import express from "express";
 import cors from "cors";
+import authRoutes from "./routes/auth.routes";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/auth", authRoutes);
+
 app.get("/", (req, res) => {
-  res.status(200).json({
-    message: "RideAlong Backend API is running 🚗",
-  });
+  res.send("API is running");
 });
 
 export default app;
